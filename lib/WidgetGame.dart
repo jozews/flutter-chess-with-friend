b994ac4debfb08f70b692b230136b986fdbf31d8
Jože Ws <joze.ws11@gmail.com>
2019-03-01 13:36:25 +0100

    fixes padding?

--- lib/WidgetGame.dart
+++ lib/WidgetGame.dart
@@ -141,7 +141,7 @@ class WidgetGameState extends State<WidgetGame> {
   // UTIL
   // ...
   // ...
-  double get heightSquare => min(MediaQuery.of(context).size.height, MediaQuery.of(context).size.width) / 8;
+  double get heightSquare => (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.vertical) / 8;
   double get heightNotation => SIZE_NOTATION + INSET_NOTATION*2;
   double get darkSpace => MediaQuery.of(context).size.width - MediaQuery.of(context).size.height;
 
@@ -160,28 +160,31 @@ class WidgetGameState extends State<WidgetGame> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: SafeArea(
-          left: true,
-          bottom: true,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                child: Row(
-                  children: <Widget>[
-                    widgetSide(),
-                    accentBoard != null ? widgetCenter() : Container(),
-                    widgetSide(atLeft: false),
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.center,
+      child: Container(
+        child: SafeArea(
+            left: true,
+            bottom: true,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      widgetSide(),
+                      accentBoard != null ? widgetCenter() : Container(),
+                      widgetSide(atLeft: false),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  color: colorBackground2,
                 ),
-                color: colorBackground2,
-              ),
-              isAlertShowing || isSettingsShowing ? widgetDim() : Container(),
-              isAlertShowing ? widgetAlert() : Container(),
-              isSettingsShowing ? widgetSettings() : Container(),
-              widgetIconSettings(),
-            ],
-          )
+                isAlertShowing || isSettingsShowing ? widgetDim() : Container(),
+                isAlertShowing ? widgetAlert() : Container(),
+                isSettingsShowing ? widgetSettings() : Container(),
+                widgetIconSettings(),
+              ],
+            )
+        ),
+        color: colorBackground1,
       ),
     );
   }
